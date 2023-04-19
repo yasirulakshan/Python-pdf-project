c2b45dcf5bdb0c1e3adfa31bf82d6f37708c34c2
Yasiru Lakshan <59885914+yasirulakshan@users.noreply.github.com>
2023-04-19 11:59:03 +0530

    gpt 3.5 integration successfull

--- src/consumer.py
+++ src/consumer.py
@@ -16,22 +16,34 @@ def searchText(faiss_index, question):
     return results
 
 
+# def getExactAnswer(result, question):
+#     prompt = """You are an AI assistant in a conversation with a human. \n Answer their questions as truthfully as possible using the provided text. If the answer is not contained within the text, say "I haven't that knowlege.". """ + \
+#         result + "\n\n Q:" + question + "\n"
+#     answer = openai.Completion.create(
+#         prompt=prompt,
+#         temperature=0.7,
+#         max_tokens=300,
+#         model="text-davinci-003",
+#         top_p=1,
+#     )["choices"][0]["text"].strip(" \n")
+#     return answer
+
 def getExactAnswer(result, question):
     prompt = """You are an AI assistant in a conversation with a human. \n Answer their questions as truthfully as possible using the provided text. If the answer is not contained within the text, say "I haven't that knowlege.". """ + \
-        result + "\n\n Q:" + question + "\n"
-    answer = openai.Completion.create(
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=300,
-        model="text-davinci-003",
-        top_p=1,
-    )["choices"][0]["text"].strip(" \n")
-    return answer
+        result
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": question},
+        ]
+    )
+
+    return completion.choices[0].message.content
 
 
 def askQuestion(question):
     faiss_index = loadIndex("index")
     result = searchText(faiss_index, question)
     answer = getExactAnswer(result, question)
-    answer = answer.lstrip("A: ")
     return answer
