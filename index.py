8b0d83ed91c6bfb5b9332affd77be3f1f801da45
Srilal Sachintha <SrilalSachintha@Gmail.com>
2023-04-19 19:39:55 +0530

    milvus completed.

--- index.py
+++ index.py
@@ -1,73 +1,24 @@
-from langchain.document_loaders import PagedPDFSplitter
-from langchain.vectorstores import FAISS
-from langchain.embeddings.openai import OpenAIEmbeddings
-import openai
 from flask import Flask, request
 from flask_cors import CORS
+import functions
 
 app = Flask(__name__)
+
 CORS(app, supports_credentials=True)
-
-
-def pageSplit(path):
-    loader = PagedPDFSplitter(path)
-    pages = loader.load_and_split()
-    return pages
-
-
-def indexing(pages):
-    faiss_index = FAISS.from_documents(pages, OpenAIEmbeddings())
-    return faiss_index
-
-
-def saveIndex(faiss_index, path):
-    faiss_index.save_local(path)
-
-
-def loadIndex(path):
-    faiss_index = FAISS.load_local(path, OpenAIEmbeddings())
-    return faiss_index
-
-
-def searchText(faiss_index, question):
-    docs = faiss_index.similarity_search(question, k=2)
-    results = ""
-    for doc in docs:
-        results += doc.page_content
-    return results
-
-
-def getExactAnswer(result, question):
-    prompt = """You are an AI assistant in a conversation with a human. \n Answer their questions as truthfully as possible using the provided text. If the answer is not contained within the text, say "I haven't that knowlege.". """ + \
-        result + "\n\n Q:" + question + "\n"
-    answer = openai.Completion.create(
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=300,
-        model="text-davinci-003",
-        top_p=1,
-    )["choices"][0]["text"].strip(" \n")
-    return answer
 
 
 @app.route("/train")
 def pdfScan():
-    pages = pageSplit("./99x-awardsv2.pdf")
-    faiss_index = indexing(pages)
-    saveIndex(faiss_index, "index")
-    return "Indexing Done"
+    pages = functions.pageSplit("./99x-awardsv2.pdf")
+    functions.indexing(pages)
+    return {
+        'status': 'Indexing Completed.'
+    }
 
 
-@app.route("/askQuestion", methods=["POST"])
+@app.route("/ask-question", methods=["POST"])
 def askQuestion():
     question = request.json["question"]
-    faiss_index = loadIndex("index")
-    result = searchText(faiss_index, question)
-    answer = getExactAnswer(result, question)
-    answer = answer.lstrip("A: ")
+    result = functions.searchText(question)
+    answer = functions.getExactAnswer(result, question)
     return answer
-
-
-@app.route("/")
-def sayHello():
-    return "Hello World"
